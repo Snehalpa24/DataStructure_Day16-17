66c104760b336d4a3dbaab3a4d11aac73bd52d62
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-08 15:02:06 +0530

    Program.cs for Binary search tree

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace DataStructureUsingBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.Display();
+            binarySearchTree.getSize();
+            bool result=binarySearchTree.ifExists(63, binarySearchTree);
+            Console.WriteLine(result);
+        }
+    }
+}
