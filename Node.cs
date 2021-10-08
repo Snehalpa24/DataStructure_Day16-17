58f031cb7707b11ff97566fd43bd93cb3f5d0a9a
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-08 15:09:55 +0530

    Node is added

---
+++ Node.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureUsingLinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        /// <summary>
+        /// Parameterized constructor to Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
